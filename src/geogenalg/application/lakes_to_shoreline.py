5eab94a5aaf8f6e547a3a9c3cf8d6ebcca21da25
Juho Ervasti <juho.ervasti@gispo.fi>
2025-06-10 14:42:50 +0300

    feat: add lakes to shoreline algorithm

---
+++ src/geogenalg/application/lakes_to_shoreline.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+from itertools import combinations
+
+import geopandas as gpd
+
+
+def extract_shoreline_from_generalized_lakes(
+    original_shoreline: gpd.GeoDataFrame,
+    lakes: gpd.GeoDataFrame,
+) -> gpd.GeoDataFrame:
+    """Extracts the boundaries from the lakes and calculates their
+    shoreline type from the original shoreline.
+
+    Returns a GeoDataFrame of the generalized shoreline.
+    """
+    gdf = gpd.GeoDataFrame(
+        {"lake_id": lakes.index}, geometry=lakes.boundary, crs=lakes.crs
+    )
+
+    buffered_shoreline = gpd.GeoDataFrame(
+        original_shoreline["shoreline_type_id"],
+        geometry=original_shoreline.buffer(10),
+        crs=original_shoreline.crs,
+    )
+
+    # NOTE: The buffering uses a round cap style instead of a flat one because with a flat one
+    # you get small gaps between the buffered sections leading to null values and extra vertices
+    # in the proceeding identity operation, which are more complex to address. By buffering over
+    # the end points you can make the buffered sections have overlapping sections which can then be
+    # removed, ensuring topological correctness.
+
+    # NOTE: As a downside, this method can cause the start and end points of a section with a
+    # different category to its surroundings to shift by about the distance which is used for
+    # the buffer operation. However the mismatch is hardly noticeable on the scale this algorithm
+    # is intended for (1: 50 000) so with the additional complexity arising from the flat cap
+    # style this seemed like the better option.
+
+    bufgeom = buffered_shoreline.geometry
+    for i_p1, i_p2 in combinations(bufgeom.index, 2):
+        if bufgeom.loc[i_p1].intersects(bufgeom.loc[i_p2]):
+            bufgeom.loc[i_p2] -= bufgeom.loc[i_p1]
+
+    gdf = (
+        gpd.overlay(gdf, buffered_shoreline, how="identity")
+        .dissolve(by=["lake_id", "shoreline_type_id"], as_index=False)
+        .drop("lake_id", axis=1)
+    )
+
+    gdf["geometry"] = gdf.geometry.remove_repeated_points()
+    gdf["geometry"] = gdf.geometry.line_merge()
+
+    return gdf.explode()  # pyright: ignore[reportCallIssue]
---
+++ test/application/test_lakes_to_shoreline.py
@@ -0,0 +1,39 @@
+import tempfile
+from pathlib import Path
+
+import geopandas as gpd
+import geopandas.testing
+
+from geogenalg.application.lakes_to_shoreline import (
+    extract_shoreline_from_generalized_lakes,
+)
+
+
+def test_extract_shoreline_from_generalized_lakes(
+    lakes_to_shoreline_sourcedata_path: Path,
+) -> None:
+    temp_dir = tempfile.TemporaryDirectory()
+    output_path = temp_dir.name + "/shoreline.gpkg"
+
+    original_shoreline = gpd.read_file(
+        lakes_to_shoreline_sourcedata_path,
+        layer="original_shoreline",
+    )
+    generalized_lakes = gpd.read_file(
+        lakes_to_shoreline_sourcedata_path,
+        layer="generalized_lakes",
+    )
+    control: gpd.GeoDataFrame = gpd.read_file(
+        lakes_to_shoreline_sourcedata_path,
+        layer="control",
+    )
+
+    res = extract_shoreline_from_generalized_lakes(
+        original_shoreline,
+        generalized_lakes,
+    )
+
+    res.to_file(output_path)
+    result = gpd.read_file(output_path)
+
+    geopandas.testing.assert_geodataframe_equal(result, control)
