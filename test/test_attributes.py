32240d92bd85bdd42a1a8caed250ed7d009c8216
eemilkoskinen <eemilkoskinen@users.noreply.github.com>
2025-06-19 13:08:31 +0300

    test: add tests for attributes and merge

--- test/test_attributes.py
+++ test/test_attributes.py
@@ -7,13 +7,13 @@
 
 import geopandas as gpd
 import pytest
-from shapely.geometry import Polygon
+from shapely.geometry import LineString, Polygon
 
 from geogenalg import attributes
 
 
 @pytest.mark.parametrize(
-    ("source_gdf", "target_gdf", "expected_attribute_value"),
+    ("source_gdf", "target_gdf", "expected_attribute_value", "expected_geometry"),
     [
         (
             gpd.GeoDataFrame(
@@ -22,14 +22,20 @@ from geogenalg import attributes
                 crs="EPSG:3067",
             ),
             gpd.GeoDataFrame(
+                {"id": [23], "mtk_id": ["009312"], "class": ["cultivated_land"]},
                 geometry=[Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])],
                 crs="EPSG:3067",
             ),
             "3491230",
+            Polygon([(0, 0), (1, 0), (1, 1), (0, 1)]),
         ),
         (
             gpd.GeoDataFrame(
-                {"id": [1, 2, 3], "mtk_id": ["3491230", "344531", "1234"]},
+                {
+                    "id": [1, 2, 3],
+                    "mtk_id": ["3491230", "344531", "1234"],
+                    "class": ["park", "park", "park"],
+                },
                 geometry=[
                     Polygon([(0, 0), (0.5, 0), (0.5, 0.5), (0, 1)]),
                     Polygon([(1, 1), (1.5, 1), (1.5, 1.5), (1, 1.5)]),
@@ -43,16 +49,60 @@ from geogenalg import attributes
                 crs="EPSG:3067",
             ),
             ["3491230", "1234"],
+            Polygon([(0, 0), (1, 0), (1, 1), (0, 1)]),
+        ),
+        (
+            gpd.GeoDataFrame(
+                {"id": [1], "mtk_id": ["888888"]},
+                geometry=[Polygon([(10, 10), (11, 10), (11, 11), (10, 11)])],
+                crs="EPSG:3067",
+            ),
+            gpd.GeoDataFrame(
+                {"id": [99], "mtk_id": ["555666"]},
+                geometry=[Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])],
+                crs="EPSG:3067",
+            ),
+            "888888",
+            Polygon([(0, 0), (1, 0), (1, 1), (0, 1)]),
+        ),
+        (
+            gpd.GeoDataFrame(
+                {
+                    "id": [1, 2, 3],
+                    "mtk_id": ["123", "456", "789"],
+                },
+                geometry=[
+                    LineString([(0, 0), (0.5, 0)]),
+                    LineString([(0.5, 0), (1, 0)]),
+                    LineString([(12, 3), (13, 4)]),
+                ],
+                crs="EPSG:3067",
+            ),
+            gpd.GeoDataFrame(
+                {"id": [42], "class": ["fence"]},
+                geometry=[LineString([(0, 0), (1, 0)])],
+                crs="EPSG:3067",
+            ),
+            ["123", "456"],
+            LineString([(0, 0), (1, 0)]),
         ),
     ],
-    ids=["same_polygon", "two_partly_intersecting_polygons"],
+    ids=[
+        "same_polygon",
+        "two_partly_intersecting_polygons",
+        "disjoint_polygon_geometries",
+        "two_partly_intersecting_linestrings",
+    ],
 )
 def test_inherit_attributes(
     source_gdf: gpd.GeoDataFrame,
     target_gdf: gpd.GeoDataFrame,
     expected_attribute_value: str,
+    expected_geometry: Polygon | LineString,
 ):
-    result = attributes.inherit_attributes(source_gdf, target_gdf)
-    assert "mtk_id" in result.columns
-    assert result.iloc[0]["mtk_id"] in expected_attribute_value
-    assert len(result) == len(target_gdf)
+    result_gdf = attributes.inherit_attributes(source_gdf, target_gdf)
+    assert "mtk_id" in result_gdf.columns
+    assert result_gdf.iloc[0]["mtk_id"] in expected_attribute_value
+    assert result_gdf.iloc[0].geometry.equals(expected_geometry)
+    assert len(result_gdf) == len(target_gdf)
+    assert set(source_gdf.columns) == set(result_gdf.columns)
