806ab4102e9cdf90d12daa4e76d907891fdee6d1
eemilkoskinen <eemilkoskinen@users.noreply.github.com>
2025-07-21 15:22:13 +0300

    feat: add function for reducing nearby points

---
+++ src/geogenalg/cluster.py
@@ -0,0 +1,40 @@
+#  Copyright (c) 2025 National Land Survey of Finland (Maanmittauslaitos)
+#
+#  This file is part of geogen-algorithms.
+#
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+
+import geopandas as gpd
+
+
+def reduce_nearby_points(
+    input_gdf: gpd.GeoDataFrame, reduce_threshold: float
+) -> gpd.GeoDataFrame:
+    """Reduce the number of points by clustering and replacing them with their centroid.
+
+    Args:
+    ----
+        input_gdf: Input GeoDataFrame with point geometries
+        reduce_threshold: Distance used for buffering and clustering
+
+    Returns:
+    -------
+        A GeoDataFrame containing centroids of clusters.
+
+    """
+    result_gdf = input_gdf.copy()
+
+    result_gdf.geometry = result_gdf.buffer(reduce_threshold)
+
+    # Merge overlapping buffers into clusters
+    result_gdf = gpd.GeoDataFrame(
+        geometry=[result_gdf.geometry.union_all()], crs=input_gdf.crs
+    )
+
+    result_gdf = result_gdf.explode(index_parts=False)
+
+    # Calculate the centroid of each cluster
+    result_gdf.geometry = result_gdf.centroid
+
+    return result_gdf
