018cc0ab73acbb5e6418f79b0959915b190d8478
eemilkoskinen <eemilkoskinen@users.noreply.github.com>
2025-07-29 12:33:36 +0300

    fix: displacement function

--- src/geogenalg/displacement.py
+++ src/geogenalg/displacement.py
@@ -15,18 +15,22 @@ from geogenalg.core.exceptions import GeometryTypeError
 
 
 def displace_points(
-    input_gdf: gpd.GeoDataFrame, displace_threshold: float
+    input_gdf: gpd.GeoDataFrame,
+    displace_threshold: float,
+    iterations: int,
 ) -> gpd.GeoDataFrame:
-    """Move points apart so that no two points are closer than a threshold.
+    """Move points apart based on the given threshold.
 
-    Note: If there are more than two points located close to each other, this
-    function may need to be called iteratively to ensure that all point-to-point
-    distances exceed the threshold.
+    Note:
+        If more than two points are located close to each other, the exact
+        displace_threshold may not be achieved. Increasing the number of iterations
+        will bring the minimum distances closer to the threshold.
 
     Args:
     ----
         input_gdf: A GeoDataFrame with point geometries
-        displace_threshold: Minimum allowed distance between points
+        displace_threshold: The minimum allowed distance between points
+        iterations: The number of times to repeat displacement loop
 
     Returns:
     -------
@@ -43,32 +47,35 @@ def displace_points(
 
     result_gdf = input_gdf.copy()
 
-    original_coordinates = np.array([[geom.x, geom.y] for geom in result_gdf.geometry])
-    number_of_coordinates = len(original_coordinates)
+    for _ in range(iterations):
+        original_coordinates = np.array(
+            [[geom.x, geom.y] for geom in result_gdf.geometry]
+        )
+        number_of_coordinates = len(original_coordinates)
 
-    # Initialize displacement vectors for each point
-    displacement_vectors = np.zeros((number_of_coordinates, 2))
+        # Initialize displacement vectors for each point
+        displacement_vectors = np.zeros((number_of_coordinates, 2))
 
-    for i in range(number_of_coordinates):
-        for j in range(number_of_coordinates):
-            if i == j:
-                continue
+        for i in range(number_of_coordinates):
+            for j in range(number_of_coordinates):
+                if i == j:
+                    continue
 
-            dx = original_coordinates[i][0] - original_coordinates[j][0]
-            dy = original_coordinates[i][1] - original_coordinates[j][1]
-            distance = np.hypot(dx, dy)
+                dx = original_coordinates[i][0] - original_coordinates[j][0]
+                dy = original_coordinates[i][1] - original_coordinates[j][1]
+                distance = np.hypot(dx, dy)
 
-            # If points are too close, calculate how much to move them apart
-            if distance < displace_threshold and distance != 0:
-                move_distance = (displace_threshold - distance) / 2
-                direction = np.array([dx, dy]) / distance
-                move_vector = direction * move_distance
+                # If points are too close, calculate how much to move them apart
+                if distance < displace_threshold and distance != 0:
+                    move_distance = (displace_threshold - distance) / 2
+                    unit_vector = np.array([dx, dy]) / distance
+                    move_vector = unit_vector * move_distance
 
-                # Accumulate the displacement for the current point
-                displacement_vectors[i] += move_vector
+                    # Accumulate the displacement for the current point
+                    displacement_vectors[i] += move_vector
 
-    # Apply displacement vectors to coordinates
-    new_coordinates = original_coordinates + displacement_vectors
-    result_gdf.geometry = list(starmap(Point, new_coordinates))
+        # Apply displacement vectors to coordinates
+        new_coordinates = original_coordinates + displacement_vectors
+        result_gdf.geometry = list(starmap(Point, new_coordinates))
 
     return result_gdf
