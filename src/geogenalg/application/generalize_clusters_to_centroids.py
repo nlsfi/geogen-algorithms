68d1272144be2087fe166151135f493d013db4c7
JuhoErvasti <juho.ervasti@gispo.fi>
2025-10-07 16:08:30 +0300

    refactor: rename boulders in water and make more generic

---
+++ src/geogenalg/application/generalize_clusters_to_centroids.py
@@ -0,0 +1,170 @@
+#  Copyright (c) 2025 National Land Survey of Finland (Maanmittauslaitos)
+#
+#  This file is part of geogen-algorithms.
+#
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+
+from collections.abc import Callable
+from dataclasses import dataclass
+from hashlib import sha256
+from itertools import chain
+from typing import Any
+
+from geopandas import GeoDataFrame
+from pandas import Series, concat
+from shapely import Point, Polygon, box
+
+from geogenalg.application import BaseAlgorithm
+from geogenalg.cluster import get_cluster_centroids
+from geogenalg.core.exceptions import GeometryTypeError
+from geogenalg.utility.validation import check_gdf_geometry_type
+
+
+@dataclass(frozen=True)
+class GeneralizePointClustersAndPolygonsToCentroids(BaseAlgorithm):
+    """Reduces polygons and point clusters to single points."""
+
+    """Points within this distance of each other will be clustered."""
+    cluster_distance: float
+    """Polygons with area smaller than this will be turned into a point."""
+    polygon_min_area: float
+    """Name of column containing a unique identifier of input features."""
+    unique_id_column: str
+    """Name of column containing type of output feature."""
+    feature_type_column: str
+    """Dictionary containing keys corresponding to a column in a geodataframe
+    and a function which will aggregate the column's value when turning
+    multiple points to a cluster."""
+    aggregation_functions: dict[str, Callable[[Series], Any] | str] | None
+
+    def _process_polygons(
+        self,
+        polygons: GeoDataFrame,
+        mask_geom: Polygon,
+    ) -> GeoDataFrame:
+        def _make_centroid(geom: Polygon) -> Point:
+            centroid = geom.centroid
+
+            if geom.has_z:
+                # TODO: calculate z as mean of vertices' z?
+                centroid = Point(centroid.x, centroid.y, 0.0)
+
+            return centroid
+
+        gdf = polygons.loc[
+            (polygons.geometry.area < self.polygon_min_area)
+            & (polygons.geometry.centroid.within(mask_geom))
+        ].copy()
+
+        gdf.geometry = gdf.geometry.apply(_make_centroid)
+
+        return gdf
+
+    def execute(
+        self,
+        data: GeoDataFrame,
+        reference_data: dict[str, GeoDataFrame],
+    ) -> GeoDataFrame:
+        """Execute algorithm.
+
+        Args:
+        ----
+            data: GeoDataFrame containing Points and/or Polygons.
+            reference_data: If contains an value with they key "mask", new
+                clusters will not be generated outside of it.
+
+        Returns:
+        -------
+            GeoDataFrame with generalized data, which contains the newly
+            created cluster points and all input features which were not turned
+            into clusters. These can be distinguished by the value in the
+            column of the name set by the feature_type_column attribute.
+
+        Raises:
+        ------
+            GeometryTypeError: If data contains something other than point or
+            polygon geometries or mask data contains non-polygon geometries.
+
+        """
+        if not check_gdf_geometry_type(data, ["Point", "Polygon"]):
+            msg = (
+                "GeneralizePointClustersAndPolygonsToCentroids "
+                + "works only with Point and Polygon geometries"
+            )
+            raise GeometryTypeError(msg)
+
+        if reference_data.get("mask") is not None and not check_gdf_geometry_type(
+            reference_data["mask"],
+            [
+                "Polygon",
+                "MultiPolygon",
+            ],
+        ):
+            msg = "mask dataframe must only contain (Multi)Polygons"
+            raise GeometryTypeError(msg)
+
+        points, polygons = (
+            data.loc[data.geometry.type == "Point"],
+            data.loc[data.geometry.type == "Polygon"],
+        )
+
+        clusters_from_points = GeoDataFrame()
+        if not points.empty:
+            clusters_from_points = get_cluster_centroids(
+                points,
+                self.cluster_distance,
+                self.unique_id_column,
+                aggregation_functions=self.aggregation_functions,
+            )
+
+            clusters_from_points[self.identity_hash_column] = (
+                clusters_from_points.old_ids.apply(
+                    # TODO: MAKE SURE THE ORDER DOES NOT MATTER
+                    lambda ids: sha256("".join(ids).encode()).hexdigest(),
+                )
+            )
+
+            clusters_from_points[self.feature_type_column] = "rock_cluster_from_point"
+
+        if reference_data.get("mask") is not None:
+            mask_gdf = reference_data["mask"]
+            mask_geom = mask_gdf.geometry.union_all()
+        else:
+            # Create a mask which will include all geometries. This is slightly
+            # less efficient, but makes this function more readable and less
+            # complex.
+            x_min, y_min, x_max, y_max = data.total_bounds
+            mask_geom = box(x_min, y_min, x_max, y_max)
+
+        clusters_from_polygons = GeoDataFrame()
+        if not polygons.empty:
+            clusters_from_polygons = self._process_polygons(polygons, mask_geom)
+            clusters_from_polygons[self.feature_type_column] = (
+                "rock_cluster_from_polygon"
+            )
+
+        if not clusters_from_points.empty:
+            clusters_from_points = clusters_from_points[
+                clusters_from_points.geometry.within(mask_geom)
+            ]
+            ids_to_remove = list(chain.from_iterable(clusters_from_points["old_ids"]))
+
+            points = points.loc[~points[self.unique_id_column].isin(ids_to_remove)]
+            clusters_from_points = clusters_from_points.drop(
+                columns=["old_ids"],
+                axis=1,
+            )
+
+        if not clusters_from_polygons.empty:
+            polygons = polygons.loc[
+                (polygons.geometry.area >= self.polygon_min_area)
+                | (~polygons.geometry.centroid.within(mask_geom)),
+            ]
+
+        points[self.feature_type_column] = "unchanged_point"
+        polygons[self.feature_type_column] = "unchanged_polygon"
+
+        return GeoDataFrame(
+            concat([clusters_from_points, clusters_from_polygons, points, polygons]),
+        )
---
+++ test/application/test_generalize_clusters_to_centroids.py
@@ -0,0 +1,199 @@
+#  Copyright (c) 2025 National Land Survey of Finland (Maanmittauslaitos)
+#
+#  This file is part of geogen-algorithms.
+#
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+
+import re
+import tempfile
+from pathlib import Path
+
+import pytest
+from geopandas import GeoDataFrame, read_file
+from geopandas.testing import assert_geodataframe_equal
+from pandas import Series, concat
+from shapely import LineString, Point, box
+
+from geogenalg.application.generalize_clusters_to_centroids import (
+    GeneralizePointClustersAndPolygonsToCentroids,
+)
+from geogenalg.core.exceptions import GeometryTypeError
+
+
+@pytest.mark.parametrize(
+    (
+        "gpkg_file",
+        "input_layer",
+    ),
+    [
+        ("boulder_in_water.gpkg", "boulder_in_water"),
+        ("boulders_in_water.gpkg", "boulders_in_water"),
+    ],
+    ids=[
+        "points",
+        "polygons",
+    ],
+)
+def test_generalize_boulders_in_water(
+    testdata_path: Path,
+    gpkg_file: str,
+    input_layer: str,
+) -> None:
+    input_path = testdata_path / gpkg_file
+    input_data = read_file(input_path, layer=input_layer)
+
+    temp_dir = tempfile.TemporaryDirectory()
+    output_path = Path(temp_dir.name) / "output.gpkg"
+
+    algorithm = GeneralizePointClustersAndPolygonsToCentroids(
+        cluster_distance=30.0,
+        unique_id_column="kmtk_id",
+        polygon_min_area=1000.0,
+        feature_type_column="feature_type",
+        aggregation_functions=None,
+    )
+
+    control = read_file(input_path, layer="control")
+    mask = read_file(input_path, layer="lake_part")
+    algorithm.execute(input_data, {"mask": mask}).to_file(output_path, layer="result")
+
+    result = read_file(output_path, layer="result")
+
+    assert_geodataframe_equal(control, result)
+
+
+def test_invalid_geom_type() -> None:
+    gdf = GeoDataFrame({"id": [1]}, geometry=[LineString((Point(0, 0), Point(1, 0)))])
+
+    algorithm = GeneralizePointClustersAndPolygonsToCentroids(
+        cluster_distance=30.0,
+        unique_id_column="kmtk_id",
+        polygon_min_area=1000.0,
+        feature_type_column="feature_type",
+        aggregation_functions=None,
+    )
+
+    with pytest.raises(
+        GeometryTypeError,
+        match="GeneralizePointClustersAndPolygonsToCentroids works only with Point and Polygon geometries",
+    ):
+        algorithm.execute(data=gdf, reference_data={})
+
+
+def test_invalid_mask_geom_type() -> None:
+    gdf = GeoDataFrame({"id": [1, 2]}, geometry=[Point(0, 0), box(0, 0, 1, 1)])
+    mask_gdf = GeoDataFrame({"id": [1]}, geometry=[Point(0, 0)])
+
+    algorithm = GeneralizePointClustersAndPolygonsToCentroids(
+        cluster_distance=30.0,
+        unique_id_column="kmtk_id",
+        polygon_min_area=1000.0,
+        feature_type_column="feature_type",
+        aggregation_functions=None,
+    )
+
+    with pytest.raises(
+        GeometryTypeError,
+        match=re.escape("mask dataframe must only contain (Multi)Polygons"),
+    ):
+        algorithm.execute(data=gdf, reference_data={"mask": mask_gdf})
+
+
+def test_mixed_geom_types(testdata_path: Path) -> None:
+    temp_dir = tempfile.TemporaryDirectory()
+    output_path = Path(temp_dir.name) / "output.gpkg"
+
+    points_path = testdata_path / "boulder_in_water.gpkg"
+    points = read_file(
+        points_path,
+        layer="boulder_in_water",
+    )
+    polygons = read_file(
+        testdata_path / "boulders_in_water.gpkg",
+        layer="boulders_in_water",
+    )
+
+    mask_gdf = read_file(points_path, layer="lake_part")
+
+    mixed = GeoDataFrame(concat([points, polygons]))
+
+    algorithm = GeneralizePointClustersAndPolygonsToCentroids(
+        cluster_distance=30.0,
+        unique_id_column="kmtk_id",
+        polygon_min_area=1000.0,
+        feature_type_column="feature_type",
+        aggregation_functions=None,
+    )
+
+    control_path = testdata_path / "boulders_in_water_mixed.gpkg"
+    control = read_file(control_path, layer="control")
+
+    algorithm.execute(mixed, {"mask": mask_gdf}).to_file(output_path, layer="result")
+    result = read_file(output_path, layer="result")
+
+    assert_geodataframe_equal(control, result)
+
+
+def test_no_mask(testdata_path: Path) -> None:
+    temp_dir = tempfile.TemporaryDirectory()
+    output_path = Path(temp_dir.name) / "output.gpkg"
+
+    points_path = testdata_path / "boulder_in_water.gpkg"
+    points = read_file(
+        points_path,
+        layer="boulder_in_water",
+    )
+
+    algorithm = GeneralizePointClustersAndPolygonsToCentroids(
+        cluster_distance=30.0,
+        unique_id_column="kmtk_id",
+        polygon_min_area=1000.0,
+        feature_type_column="feature_type",
+        aggregation_functions=None,
+    )
+
+    control_path = testdata_path / "boulder_in_water.gpkg"
+    control = read_file(control_path, layer="control_no_mask")
+
+    algorithm.execute(points, {}).to_file(output_path, layer="result")
+    result = read_file(output_path, layer="result")
+
+    assert_geodataframe_equal(control, result)
+
+
+def test_aggregation_functions(testdata_path: Path) -> None:
+    def _aggregate_boulder_in_water_type(values: Series) -> int:
+        if len(set(values)) == 1:
+            return int(values.to_numpy()[0])
+
+        return 2
+
+    temp_dir = tempfile.TemporaryDirectory()
+    output_path = Path(temp_dir.name) / "output.gpkg"
+
+    points_path = testdata_path / "boulder_in_water.gpkg"
+    points = read_file(
+        points_path,
+        layer="boulder_in_water",
+    )
+
+    aggregation_functions = {
+        "boulder_in_water_type_id": _aggregate_boulder_in_water_type
+    }
+
+    algorithm = GeneralizePointClustersAndPolygonsToCentroids(
+        cluster_distance=30.0,
+        unique_id_column="kmtk_id",
+        polygon_min_area=1000.0,
+        feature_type_column="feature_type",
+        aggregation_functions=aggregation_functions,
+    )
+
+    control_path = testdata_path / "boulder_in_water.gpkg"
+    control = read_file(control_path, layer="control_aggfunc")
+
+    algorithm.execute(points, {}).to_file(output_path, layer="result")
+    result = read_file(output_path, layer="result")
+
+    assert_geodataframe_equal(control, result)
