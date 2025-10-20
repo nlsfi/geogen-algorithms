349851f149af3f0e5c4d40037eaa43451915d646
komima <58747243+komima@users.noreply.github.com>
2025-10-20 11:25:40 +0300

    feat: new api for identity support

---
+++ test/application/test_base_algorithm.py
@@ -0,0 +1,75 @@
+#  Copyright (c) 2025 National Land Survey of Finland (Maanmittauslaitos)
+#
+#  This file is part of geogen-algorithms.
+#
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+
+from geopandas import GeoDataFrame
+from pandas import Index
+from pandas.testing import assert_index_equal
+from shapely import Point
+
+from geogenalg.application import BaseAlgorithm, supports_identity
+
+
+def test_index_reset_without_identity_support():
+    class MockAlg(BaseAlgorithm):
+        def _execute(self, data, reference_data):  # noqa: ANN001, ANN202, ARG002
+            return data
+
+    input_data = GeoDataFrame(
+        {
+            "a": [1, 2, 3],
+            "b": ["x", "y", "z"],
+            "geom": [Point(0, 0), Point(1, 1), Point(2, 2)],
+        },
+        index=["abc", "def", "ghi"],
+        geometry="geom",
+    )
+
+    output_data = MockAlg().execute(input_data, {})
+
+    assert len(output_data.index.intersection(["abc", "def", "ghi"])) == 0
+
+
+def test_index_not_reset_with_identity_support():
+    @supports_identity
+    class MockAlg(BaseAlgorithm):
+        def _execute(self, data, reference_data):  # noqa: ANN001, ANN202, ARG002
+            return data
+
+    input_data = GeoDataFrame(
+        {
+            "a": [1, 2, 3],
+            "b": ["x", "y", "z"],
+            "geom": [Point(0, 0), Point(1, 1), Point(2, 2)],
+        },
+        index=["abc", "def", "ghi"],
+        geometry="geom",
+    )
+
+    output_data = MockAlg().execute(input_data, {})
+
+    assert_index_equal(output_data.index, input_data.index)
+
+
+def test_input_index_converted_to_strings_for_algorithm_use():
+    @supports_identity
+    class MockAlg(BaseAlgorithm):
+        def _execute(self, data, reference_data):  # noqa: ANN001, ANN202, ARG002
+            return data
+
+    input_data = GeoDataFrame(
+        {
+            "a": [1, 2, 3],
+            "b": ["x", "y", "z"],
+            "geom": [Point(0, 0), Point(1, 1), Point(2, 2)],
+        },
+        index=[111, 222, 333],
+        geometry="geom",
+    )
+
+    output_data = MockAlg().execute(input_data, {})
+
+    assert_index_equal(output_data.index, Index(["111", "222", "333"], dtype="string"))
