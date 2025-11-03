59fce5b88f0e05b88fe7edba5a58e6a284d3cc3a
Niko Aarnio <niko.aarnio@protonmail.com>
2025-11-03 09:29:27 +0200

    refactor: update GeneralizePoints
    Added input geometry type check, changed execute to _execute,
    added alg class documentation, changed import style in module
    and added default parameter values.

--- src/geogenalg/application/generalize_points.py
+++ src/geogenalg/application/generalize_points.py
@@ -10,34 +10,48 @@ from typing import override
 
 from geopandas import GeoDataFrame
 
-from geogenalg import cluster, displacement
 from geogenalg.application import BaseAlgorithm
+from geogenalg.cluster import reduce_nearby_points_by_clustering
+from geogenalg.core.exceptions import GeometryTypeError
+from geogenalg.displacement import displace_points
+from geogenalg.utility.validation import check_gdf_geometry_type
 
 
 @dataclass(frozen=True)
 class GeneralizePoints(BaseAlgorithm):
     """Generalizes point features by reducing and displacing them.
 
+    Reference data is not used for this algorithm.
+
+    Output contains created cluster centroids and single points which
+    were not turned into centroids. The `cluster_members_column`
+    attribute lists all points that were clustered or None if the
+    point is original point from input.
+
     Note:
         If more than two points are located close to each other, the exact
         `displace_threshold` may not be achieved. Increasing the number of iterations
         for displacing points will bring the minimum distances closer to the threshold.
 
+    The algorithm does the following steps:
+    - Reduces amount of points by clustering
+    - Moves points close to each other apart
+
     """
 
-    reduce_threshold: float
+    reduce_threshold: float = 10.0
     """Distance used for buffering and clustering points."""
-    displace_threshold: float
+    displace_threshold: float = 70.0
     """Minimum allowed distance between points after displacement."""
-    displace_points_iterations: int
+    displace_points_iterations: int = 10
     """The number of times to repeat displacement loop."""
-    unique_key_column: str
+    unique_key_column: str = "mtk_id"
     """Name of the column containing unique identifiers."""
-    cluster_members_column: str
+    cluster_members_column: str = "cluster_members"
     """Name of the column that lists the points included in each cluster."""
 
     @override
-    def execute(
+    def _execute(
         self,
         data: GeoDataFrame,
         reference_data: dict[str, GeoDataFrame],
@@ -53,14 +67,22 @@ class GeneralizePoints(BaseAlgorithm):
         -------
             A GeoDataFrame containing the generalized points.
 
+        Raises:
+        ------
+            GeometryTypeError: If `data` contains non-point geometries.
+
         """
-        clustered_points = cluster.reduce_nearby_points_by_clustering(
+        if not check_gdf_geometry_type(data, ["Point"]):
+            msg = "GeneralizePoints works only with Point geometries."
+            raise GeometryTypeError(msg)
+
+        clustered_points = reduce_nearby_points_by_clustering(
             data,
             self.reduce_threshold,
             self.unique_key_column,
             self.cluster_members_column,
         )
 
-        return displacement.displace_points(
+        return displace_points(
             clustered_points, self.displace_threshold, self.displace_points_iterations
         )
