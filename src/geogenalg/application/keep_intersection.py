d48547bebf4ddd8e971858f8169957a03420124d
JuhoErvasti <juho.ervasti@gispo.fi>
2025-11-19 16:16:35 +0200

    feat: add new KeepIntersection algorithm

---
+++ src/geogenalg/application/keep_intersection.py
@@ -0,0 +1,82 @@
+#  Copyright (c) 2025 National Land Survey of Finland (Maanmittauslaitos)
+#
+#  This file is part of geogen-algorithms.
+#
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+
+from dataclasses import dataclass
+from typing import override
+
+from geopandas import GeoDataFrame
+
+from geogenalg.application import BaseAlgorithm, supports_identity
+from geogenalg.core.exceptions import GeometryTypeError, MissingReferenceError
+from geogenalg.utility.validation import check_gdf_geometry_type
+
+
+@supports_identity
+@dataclass(frozen=True)
+class KeepIntersection(BaseAlgorithm):
+    """Keep intersecting areas from input polygon data.
+
+    Identifies which areas in input data intersect with areas in mask data
+    and returns a copy of input data with only intersecting areas remaining.
+
+    If feature in input data has zero intersection with mask, it will be
+    removed.
+
+    Keeps attributes and IDs returned geometries intact. If a polygon
+    is split by the mask, a MultiPolygon is created. New vertices introduced
+    by cuts get Z value via linear interpolation along the affected edges.
+    """
+
+    reference_key: str = "mask"
+    """Reference data key to use as a mask layer for overlap detection."""
+
+    @override
+    def _execute(
+        self,
+        data: GeoDataFrame,
+        reference_data: dict[str, GeoDataFrame],
+    ) -> GeoDataFrame:
+        """Execute algorithm.
+
+        Args:
+        ----
+            data: GeoDataFrame to be overlaid.
+            reference_data: Should contain a GeoDataFrame with
+                (multi)polygons with which to overlay input data.
+
+        Returns:
+        -------
+            A copy of `input_data` where areas outside geometries of
+            `mask_data` have been removed.
+
+        Raises:
+        ------
+            GeometryTypeError: If mask data has non-polygon geometries.
+            MissingReferenceError: If reference data is not found.
+
+        """
+        if self.reference_key not in reference_data:
+            msg = "Reference data with mask polygons is mandatory."
+            raise MissingReferenceError(msg)
+
+        mask_data = reference_data[self.reference_key]
+
+        if not check_gdf_geometry_type(mask_data, ["MultiPolygon", "Polygon"]):
+            msg = "Mask data should include only Polygon geometries."
+            raise GeometryTypeError(msg)
+
+        # Overlay does not keep index here, so copy it to a separate column,
+        # and set back as index later
+        index_name = data.index.name
+
+        gdf = data.copy()
+        gdf["__index"] = gdf.index
+        result = gdf.overlay(mask_data, how="intersection")
+        result = result.set_index("__index")
+        result.index.name = index_name
+
+        return result
---
+++ test/application/test_keep_intersection.py
@@ -0,0 +1,116 @@
+#  Copyright (c) 2025 National Land Survey of Finland (Maanmittauslaitos)
+#
+#  This file is part of geogen-algorithms.
+#
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import pytest
+from geopandas import GeoDataFrame, read_file
+from pandas.testing import assert_frame_equal
+from shapely.geometry import Point, Polygon
+
+from geogenalg.application.keep_intersection import KeepIntersection
+from geogenalg.core.exceptions import (
+    GeometryTypeError,
+    MissingReferenceError,
+)
+from geogenalg.utility.dataframe_processing import read_gdf_from_file_and_set_index
+
+UNIQUE_ID_COLUMN = "uuid"
+
+
+@pytest.mark.parametrize(
+    (
+        "gpkg_file",
+        "input_layer",
+        "layer_suffix",
+    ),
+    [
+        ("keep_intersection.gpkg", "data", "polygons"),
+        ("keep_intersection.gpkg", "data", "lines"),
+        ("keep_intersection.gpkg", "data", "points"),
+    ],
+    ids=[
+        "polygons",
+        "lines",
+        "points",
+    ],
+)
+def test_keep_intersection(
+    testdata_path: Path,
+    gpkg_file: str,
+    input_layer: str,
+    layer_suffix: str,
+) -> None:
+    input_layer = f"{input_layer}_{layer_suffix}"
+    input_path = testdata_path / gpkg_file
+    input_data = read_gdf_from_file_and_set_index(
+        input_path,
+        UNIQUE_ID_COLUMN,
+        layer=input_layer,
+    )
+
+    temp_dir = TemporaryDirectory()
+    output_path = Path(temp_dir.name) / "output.gpkg"
+
+    algorithm = KeepIntersection()
+
+    control = read_gdf_from_file_and_set_index(
+        input_path,
+        UNIQUE_ID_COLUMN,
+        layer=f"control_{layer_suffix}",
+    )
+    mask = read_file(input_path, layer="mask")
+    algorithm.execute(input_data, {"mask": mask}).to_file(output_path, layer="result")
+
+    result = read_gdf_from_file_and_set_index(
+        output_path, UNIQUE_ID_COLUMN, layer="result"
+    )
+
+    assert_frame_equal(control, result)
+
+
+def test_remove_overlap_missing_reference_data():
+    with pytest.raises(
+        MissingReferenceError,
+        match=r"Reference data with mask polygons is mandatory.",
+    ):
+        KeepIntersection().execute(
+            data=GeoDataFrame(
+                {
+                    "id": [1],
+                },
+                geometry=[Polygon([(0, 0), (0, 1), (1, 1)])],
+                crs="EPSG:4326",
+            ),
+            reference_data={},
+        )
+
+
+def test_remove_overlap_invalid_mask_geometry():
+    with pytest.raises(
+        GeometryTypeError,
+        match=r"Mask data should include only Polygon geometries.",
+    ):
+        KeepIntersection().execute(
+            data=GeoDataFrame(
+                {
+                    "id": [1],
+                },
+                geometry=[Polygon([(0, 0), (0, 1), (1, 1)])],
+                crs="EPSG:4326",
+            ),
+            reference_data={
+                "mask": GeoDataFrame(
+                    {
+                        "id": [1],
+                    },
+                    geometry=[Point(0, 0)],
+                    crs="EPSG:4326",
+                )
+            },
+        )
