06da38cb5364b46c9e2839215e1262e9d583b53a
JuhoErvasti <juho.ervasti@gispo.fi>
2025-12-05 14:55:16 +0200

    feat(identity): add hash_index_from_old_ids function

--- src/geogenalg/identity.py
+++ src/geogenalg/identity.py
@@ -7,14 +7,14 @@
 
 from hashlib import sha256
 
-from geopandas import GeoDataFrame
+from pandas import DataFrame, Index
 from pandas.api.types import is_string_dtype
 
 
 def hash_duplicate_indexes(
-    data: GeoDataFrame,
+    data: DataFrame,
     hash_prefix: str,
-) -> GeoDataFrame:
+) -> DataFrame:
     """Change any duplicate indexes to a hash value.
 
     It is required that the input has a string index.
@@ -56,5 +56,75 @@ def hash_duplicate_indexes(
 
     gdf = gdf.set_index(temp_index)
     gdf.index.name = data.index.name
+
+    return gdf
+
+
+def hash_index_from_old_ids(
+    data: DataFrame, hash_prefix: str, old_ids_column: str, *, drop_old_ids: bool = True
+) -> DataFrame:
+    """Change dataframe index to a hash value based on an old IDs column.
+
+    It is required that the input has a string index.
+
+    The old IDs may be a result of a merging rows together, and must contain a
+    tuple or a list, or a null value.
+
+    The hashing will use SHA256 and the input will be the given hash prefix and
+    all the old IDs concatenated together.
+
+    If a row has an empty or null value for old IDs, the original index is retained.
+
+    Args:
+    ----
+        data: (Geo)DataFrame to process.
+        hash_prefix: Prefix to use in hash input, can be used to identify an algorithm
+            so that two algorithms can't produce the same output.
+        old_ids_column: Name of column where old IDs are found.
+        drop_old_ids: Whether old_ids_column should be dropped after updating the index.
+
+    Returns:
+    -------
+        DataFrame with the processed index.
+
+    Raises:
+    ------
+        ValueError: If input data does not have a string index.
+        TypeError: If a non-null old IDs value is not a tuple or a list.
+
+    """
+    if not is_string_dtype(data.index):
+        msg = "GeoDataFrame must have a string index."
+        raise ValueError(msg)
+
+    gdf = data.copy()
+
+    new_index = []
+    for index, row in gdf.iterrows():
+        old_ids = row[old_ids_column]
+
+        if not old_ids:
+            new_index.append(index)
+            continue
+
+        if not isinstance(old_ids, tuple | list):
+            msg = "Column with old ids should contain a list or a tuple."
+            raise TypeError(msg)
+
+        if len(old_ids) == 1:
+            new_index.append(index)
+            continue
+
+        new_index.append(
+            sha256(
+                hash_prefix.encode() + "".join(sorted(old_ids)).encode(),
+            ).hexdigest()
+        )
+
+    gdf.index = Index(new_index)
+    gdf.index.name = data.index.name
+
+    if drop_old_ids:
+        gdf = gdf.drop(old_ids_column, axis=1)
 
     return gdf
--- test/test_identity.py
+++ test/test_identity.py
@@ -10,7 +10,7 @@ from geopandas import GeoDataFrame
 from geopandas.testing import assert_geodataframe_equal
 from shapely import Point
 
-from geogenalg.identity import hash_duplicate_indexes
+from geogenalg.identity import hash_duplicate_indexes, hash_index_from_old_ids
 
 
 @pytest.mark.parametrize(
@@ -104,6 +104,126 @@ def test_hash_duplicate_indexes(
     control = control.set_index("id")
 
     result = hash_duplicate_indexes(input_data, hash_prefix)
+
+    assert_geodataframe_equal(
+        result,
+        control,
+    )
+
+
+@pytest.mark.parametrize(
+    ("input_data", "hash_prefix", "drop_old_ids", "control"),
+    [
+        (
+            GeoDataFrame(
+                {
+                    "id": ["1", "2", "3"],
+                    "old_ids": [("9", "10"), None, ("3",)],
+                },
+                geometry=[
+                    Point(0, 0),
+                    Point(1, 0),
+                    Point(2, 0),
+                ],
+            ),
+            "test",
+            True,
+            GeoDataFrame(
+                {
+                    "id": [
+                        "6d54142c2eee9dd273f8b512a4cb7324a361b6e8444945c51458b14d407dce0e",
+                        "2",
+                        "3",
+                    ],
+                },
+                geometry=[
+                    Point(0, 0),
+                    Point(1, 0),
+                    Point(2, 0),
+                ],
+            ),
+        ),
+        (
+            GeoDataFrame(
+                {
+                    "id": ["1", "2", "3"],
+                    "old_ids": [("9", "10"), None, ("3",)],
+                },
+                geometry=[
+                    Point(0, 0),
+                    Point(1, 0),
+                    Point(2, 0),
+                ],
+            ),
+            "test",
+            False,
+            GeoDataFrame(
+                {
+                    "id": [
+                        "6d54142c2eee9dd273f8b512a4cb7324a361b6e8444945c51458b14d407dce0e",
+                        "2",
+                        "3",
+                    ],
+                    "old_ids": [("9", "10"), None, ("3",)],
+                },
+                geometry=[
+                    Point(0, 0),
+                    Point(1, 0),
+                    Point(2, 0),
+                ],
+            ),
+        ),
+        (
+            GeoDataFrame(
+                {
+                    "id": ["1", "2", "3"],
+                    "old_ids": [("9", "10"), None, ("3",)],
+                },
+                geometry=[
+                    Point(0, 0),
+                    Point(1, 0),
+                    Point(2, 0),
+                ],
+            ),
+            "different",
+            True,
+            GeoDataFrame(
+                {
+                    "id": [
+                        "a432ccc62ec30628f7953fe5123fa1a8765d8bb28e3d9a38bbeb80309c6efd90",
+                        "2",
+                        "3",
+                    ],
+                },
+                geometry=[
+                    Point(0, 0),
+                    Point(1, 0),
+                    Point(2, 0),
+                ],
+            ),
+        ),
+    ],
+    ids=[
+        "one hash value",
+        "one hash value, different hash prefix",
+        "one hash value, don't drop old ids",
+    ],
+)
+def test_hash_index_from_old_ids(
+    input_data: GeoDataFrame,
+    hash_prefix: str,
+    drop_old_ids: bool,
+    control: GeoDataFrame,
+):
+    input_data = input_data.set_index("id")
+    control = control.set_index("id")
+
+    result = hash_index_from_old_ids(
+        input_data,
+        hash_prefix,
+        "old_ids",
+        drop_old_ids=drop_old_ids,
+    )
 
     assert_geodataframe_equal(
         result,
