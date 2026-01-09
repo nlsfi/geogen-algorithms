1b85281a90d7ab9051771271bfcbef520a372db1
JuhoErvasti <juho.ervasti@gispo.fi>
2026-01-09 15:18:44 +0200

    chore: use SPDX license ids

--- src/geogenalg/utility/hash.py
+++ src/geogenalg/utility/hash.py
@@ -2,8 +2,7 @@
 #
 #  This file is part of geogen-algorithms.
 #
-#  This source code is licensed under the MIT license found in the
-#  LICENSE file in the root directory of this source tree.
+#  SPDX-License-Identifier: MIT
 
 from secrets import token_hex
 from typing import TypeVar, cast
