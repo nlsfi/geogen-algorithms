6c634a987d56e355e31e0930b65538b27e553e2e
JuhoErvasti <juho.ervasti@gispo.fi>
2026-01-26 17:24:51 +0200

    test: refactor cliffs to use IntegrationTest

--- test/application/test_generalize_cliffs.py
+++ test/application/test_generalize_cliffs.py
@@ -4,68 +4,26 @@
 #
 #  SPDX-License-Identifier: MIT
 
-import re
 from pathlib import Path
 
-import pytest
-from geopandas import GeoDataFrame
-from pandas.testing import assert_frame_equal
-from shapely import Point
+from conftest import IntegrationTest
 
 from geogenalg.application.generalize_cliffs import GeneralizeCliffs
-from geogenalg.core.exceptions import GeometryTypeError, MissingReferenceError
-from geogenalg.testing import GeoPackageInput, get_test_gdfs
-from geogenalg.utility.dataframe_processing import read_gdf_from_file_and_set_index
+from geogenalg.testing import GeoPackagePath
 
 UNIQUE_ID_COLUMN = "mtk_id"
 
 
-def test_generalize_cliffs_50k(testdata_path: Path) -> None:
-    input_path = testdata_path / "cliffs.gpkg"
-    _, _, result, control = get_test_gdfs(
-        GeoPackageInput(input_path, layer_name="cliffs_source"),
-        GeoPackageInput(input_path, layer_name="control"),
-        GeneralizeCliffs(buffer_size=20, length_threshold=50, reference_key="roads"),
-        UNIQUE_ID_COLUMN,
-        reference_uris={"roads": GeoPackageInput(input_path, layer_name="roads")},
-    )
+def test_generalize_cliffs(testdata_path: Path) -> None:
+    gpkg = GeoPackagePath(testdata_path / "cliffs.gpkg")
 
-    assert_frame_equal(result, control)
-
-
-def test_generalize_cliffs_invalid_geometry_type() -> None:
-    with pytest.raises(GeometryTypeError):
-        GeneralizeCliffs().execute(
-            data=GeoDataFrame(geometry=[Point(0, 0)]), reference_data={}
-        )
-
-
-def test_generalize_cliffs_missing_reference_data(testdata_path: Path) -> None:
-    input_path = testdata_path / "cliffs.gpkg"
-    input_data = read_gdf_from_file_and_set_index(
-        input_path,
-        UNIQUE_ID_COLUMN,
-        layer="cliffs_source",
-    )
-
-    with pytest.raises(
-        MissingReferenceError, match=re.escape("Reference data is missing.")
-    ):
-        GeneralizeCliffs().execute(data=input_data, reference_data={})
-
-
-def test_generalize_cliffs_invalid_reference_data_geometry_type(
-    testdata_path: Path,
-) -> None:
-    input_path = testdata_path / "cliffs.gpkg"
-    input_data = read_gdf_from_file_and_set_index(
-        input_path,
-        UNIQUE_ID_COLUMN,
-        layer="cliffs_source",
-    )
-
-    with pytest.raises(GeometryTypeError):
-        GeneralizeCliffs().execute(
-            data=input_data,
-            reference_data={"roads": GeoDataFrame(geometry=[Point(0, 0)])},
-        )
+    IntegrationTest(
+        input_uri=gpkg.to_input("cliffs_source"),
+        control_uri=gpkg.to_input("control"),
+        algorithm=GeneralizeCliffs(),
+        unique_id_column=UNIQUE_ID_COLUMN,
+        check_missing_reference=True,
+        reference_uris={
+            "roads": gpkg.to_input("roads"),
+        },
+    ).run()
