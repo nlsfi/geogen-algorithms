fe2ac31a6f0277937d942a00d8a5a25e4b0f1f75
JuhoErvasti <juho.ervasti@gispo.fi>
2026-01-26 16:52:04 +0200

    chore: use spdx license id in test files

--- test/application/test_generalize_landcover.py
+++ test/application/test_generalize_landcover.py
@@ -2,8 +2,7 @@
 #
 #  This file is part of geogen-algorithms.
 #
-#  This source code is licensed under the MIT license found in the
-#  LICENSE file in the root directory of this source tree.
+#  SPDX-License-Identifier: MIT
 
 from pathlib import Path
 from tempfile import TemporaryDirectory
