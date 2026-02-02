70bf26edbeddb1efaf0ce711e8e9b5e48ec3ea27
JuhoErvasti <juho.ervasti@gispo.fi>
2026-02-02 14:28:27 +0200

    test: refactor remove overlap to use IntegrationTest

--- src/geogenalg/application/remove_overlap.py
+++ src/geogenalg/application/remove_overlap.py
@@ -3,7 +3,6 @@
 #  This file is part of geogen-algorithms.
 #
 #  SPDX-License-Identifier: MIT
-
 from dataclasses import dataclass
 from typing import ClassVar, override
 
@@ -11,6 +10,7 @@ from geopandas import GeoDataFrame
 
 from geogenalg.application import BaseAlgorithm, supports_identity
 from geogenalg.core.exceptions import MissingReferenceError
+from geogenalg.split import explode_and_hash_id
 
 
 @supports_identity
@@ -29,8 +29,18 @@ class RemoveOverlap(BaseAlgorithm):
     reference_key: str = "mask"
     """Reference data key to use as a mask layer for overlap detection."""
 
-    valid_input_geometry_types: ClassVar = {"Polygon"}
-    valid_reference_geometry_types: ClassVar = {"Polygon"}
+    valid_input_geometry_types: ClassVar = {
+        "LineString",
+        "Polygon",
+        "MultiLineString",
+        "MultiPolygon",
+        "Point",
+        "MultiPoint",
+        "LinearRing",
+        "GeometryCollection",
+    }
+
+    valid_reference_geometry_types: ClassVar = {"Polygon", "MultiPolygon"}
 
     @override
     def _execute(
@@ -38,27 +48,25 @@ class RemoveOverlap(BaseAlgorithm):
         data: GeoDataFrame,
         reference_data: dict[str, GeoDataFrame],
     ) -> GeoDataFrame:
-        """Execute algorithm.
-
-        Args:
-        ----
-            data: GeoDataFrame with (multi)polygons to be overlaid.
-            reference_data: Should contain a GeoDataFrame with
-                (multi)polygons with which to overlay input data.
-
-        Returns:
-        -------
-            A copy of `input_data` where areas overlapping with geometries of
-            `mask_data` have been removed.
-
-        Raises:
-        ------
-            MissingReferenceError: If reference data is not found.
-
-        """
         if self.reference_key not in reference_data:
             raise MissingReferenceError
 
         mask_data = reference_data[self.reference_key]
 
-        return data.overlay(mask_data, how="difference")
+        # Overlay does not keep index here, so copy it to a separate column,
+        # and set back as index later
+        index_name = data.index.name
+
+        # Create GeoDataFrame with only geometries for mask so its attributes
+        # are not inherited.
+        mask_gdf = GeoDataFrame(mask_data.geometry)
+
+        gdf = data.copy()
+        gdf["__index"] = gdf.index
+        result = gdf.overlay(mask_gdf, how="difference")
+        result = result.set_index("__index")
+        result.index.name = index_name
+
+        # Sometimes gdf.overlay results in multiple single geometries, sometimes
+        # in multigeometries. Therefore explode and hash.
+        return explode_and_hash_id(result, "removeoverlap")
--- test/application/test_remove_overlap.py
+++ test/application/test_remove_overlap.py
@@ -4,125 +4,43 @@
 #
 #  SPDX-License-Identifier: MIT
 
+from pathlib import Path
+
 import pytest
-from geopandas import GeoDataFrame
-from pandas.testing import assert_series_equal
-from shapely.geometry import MultiPolygon, Point, Polygon
+from conftest import IntegrationTest
 
 from geogenalg.application.remove_overlap import RemoveOverlap
-from geogenalg.core.exceptions import (
-    GeometryTypeError,
-    MissingReferenceError,
+from geogenalg.testing import GeoPackagePath
+
+UNIQUE_ID_COLUMN = "uuid"
+
+
+@pytest.mark.parametrize(
+    ("layer_suffix"),
+    [
+        ("polygons"),
+        ("lines"),
+        ("points"),
+    ],
+    ids=[
+        "polygons",
+        "lines",
+        "points",
+    ],
 )
+def test_remove_overlap(
+    testdata_path: Path,
+    layer_suffix: str,
+) -> None:
+    gpkg = GeoPackagePath(testdata_path / "remove_overlap.gpkg")
 
-
-def test_remove_overlap():
-    input_polygons = [
-        Polygon([(0, 0, 1), (0, 4, 1), (4, 4, 1), (4, 0, 1)]),
-        Polygon([(10, 0, 5), (10, 2, 5), (8, 2, 5), (8, 0, 5)]),  # untouched polygon
-    ]
-    input_gdf = GeoDataFrame(
-        {"id": [1122, 2233], "name": ["A", "B"], "value": [10, 20]},
-        geometry=input_polygons,
-        crs="EPSG:4326",
-    )
-    input_gdf.set_index("id")
-
-    algorithm = RemoveOverlap(reference_key="mask")
-
-    # CASE 1: Mask polygon overlaps a section of input polygon
-    mask_poly = Polygon([(0, 3), (0, 4), (4, 4), (4, 3)])
-    mask_gdf = GeoDataFrame(
-        {"id": [1], "foo": ["bar"]}, geometry=[mask_poly], crs="EPSG:4326"
-    )
-
-    out = algorithm.execute(input_gdf, {"mask": mask_gdf})
-    assert len(out) == 2
-    assert type(out[out["name"] == "A"].iloc[0].geometry) is Polygon
-    assert type(out[out["name"] == "B"].iloc[0].geometry) is Polygon
-
-    # Check that polygon B from input is untouched since it did not overlap with mask polygon
-    out_b = out[out["name"] == "B"].iloc[0].geometry
-    original_b = input_gdf[input_gdf["name"] == "B"].iloc[0].geometry
-    assert out_b == original_b
-
-    # Check that polygon A that was cut has smaller area now
-    out_a = out[out["name"] == "A"].iloc[0].geometry
-    original_a = input_gdf[input_gdf["name"] == "A"].iloc[0].geometry
-    assert out_a.area < original_a.area
-
-    # Verify that Z value (1) is preserved for unchanged vertices
-    assert {round(z, 3) for _, _, z in out_a.exterior.coords} == {1}
-
-    # CASE 2: Mask polygon overlaps a section of input polygon so that it gets split
-    mask_poly_2 = Polygon([(0, 2), (0, 3), (4, 3), (4, 2)])
-    mask_gdf_2 = GeoDataFrame(
-        {"id": [1], "foo": ["bar"]}, geometry=[mask_poly_2], crs="EPSG:4326"
-    )
-
-    out = algorithm.execute(input_gdf, {"mask": mask_gdf_2})
-    assert (
-        type(out[out["name"] == "A"].iloc[0].geometry) is MultiPolygon
-    )  # Polygon was split into parts
-
-    # Value column should be preserved from input
-    assert_series_equal(out["value"], input_gdf["value"])
-
-
-def test_remove_overlap_invalid_input_geometry():
-    with pytest.raises(
-        GeometryTypeError,
-        match=r"Input data must contain only geometries of following types: Polygon.",
-    ):
-        RemoveOverlap().execute(
-            data=GeoDataFrame(
-                {
-                    "id": [1],
-                },
-                geometry=[Point(0, 0)],
-                crs="EPSG:4326",
-            ),
-            reference_data={"mask": GeoDataFrame()},
-        )
-
-
-def test_remove_overlap_missing_reference_data():
-    with pytest.raises(
-        MissingReferenceError,
-        match=r"Reference data is missing.",
-    ):
-        RemoveOverlap().execute(
-            data=GeoDataFrame(
-                {
-                    "id": [1],
-                },
-                geometry=[Polygon([(0, 0), (0, 1), (1, 1)])],
-                crs="EPSG:4326",
-            ),
-            reference_data={},
-        )
-
-
-def test_remove_overlap_invalid_mask_geometry():
-    with pytest.raises(
-        GeometryTypeError,
-        match=r"Reference data must contain only geometries of following types: Polygon.",
-    ):
-        RemoveOverlap().execute(
-            data=GeoDataFrame(
-                {
-                    "id": [1],
-                },
-                geometry=[Polygon([(0, 0), (0, 1), (1, 1)])],
-                crs="EPSG:4326",
-            ),
-            reference_data={
-                "mask": GeoDataFrame(
-                    {
-                        "id": [1],
-                    },
-                    geometry=[Point(0, 0)],
-                    crs="EPSG:4326",
-                )
-            },
-        )
+    IntegrationTest(
+        input_uri=gpkg.to_input(f"data_{layer_suffix}"),
+        control_uri=gpkg.to_input(f"control_{layer_suffix}"),
+        algorithm=RemoveOverlap(),
+        unique_id_column=UNIQUE_ID_COLUMN,
+        reference_uris={
+            "mask": gpkg.to_input("mask"),
+        },
+        check_missing_reference=True,
+    ).run()
