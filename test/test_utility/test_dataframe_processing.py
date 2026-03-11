5f401db2fab7e930a760c717a6038b062a90b79b
Juho <juho.ervasti@gispo.fi>
2026-03-11 13:32:49 +0200

    feat: add combine_gdfs and combine_geoseries

--- test/test_utility/test_dataframe_processing.py
+++ test/test_utility/test_dataframe_processing.py
@@ -3,17 +3,24 @@
 #  This file is part of geogen-algorithms.
 #
 #  SPDX-License-Identifier: MIT
-
-
+import re
 from pathlib import Path
 
 import pytest
 from geopandas import GeoDataFrame
-from geopandas.testing import assert_geodataframe_equal
+from geopandas.geoseries import GeoSeries
+from geopandas.testing import assert_geodataframe_equal, assert_geoseries_equal
+from pandas import DataFrame, Series
 from pandas.testing import assert_frame_equal
 from shapely.geometry import LineString, Point, Polygon
 
+from geogenalg.core.exceptions import GeoCombineError
 from geogenalg.utility import dataframe_processing
+from geogenalg.utility.dataframe_processing import (
+    ConcatParameters,
+    combine_gdfs,
+    combine_geoseries,
+)
 
 
 def test_read_gdf_from_file_and_set_index(testdata_path: Path):
@@ -133,3 +140,242 @@ def test_group_gdfs_by_geometry_type(
         result_gdf = result_gdf.reset_index(drop=True)
 
         assert_frame_equal(result_gdf, expected_gdf, check_dtype=False)
+
+
+@pytest.mark.parametrize(
+    ("inputs", "message"),
+    [
+        (
+            [DataFrame()],
+            "Non-GeoDataFrame object found.",
+        ),
+        (
+            [GeoDataFrame(geometry=[], crs="EPSG:3857"), DataFrame()],
+            "Non-GeoDataFrame object found.",
+        ),
+        (
+            [GeoDataFrame()],
+            "GeoDataFrame does not have active geometry column set.",
+        ),
+        (
+            [
+                GeoDataFrame(geometry=[], crs="EPSG:3857"),
+                GeoDataFrame(geometry=[], crs="EPSG:4326"),
+            ],
+            "Different CRSs found in GeoDataFrames.",
+        ),
+        (
+            [
+                GeoDataFrame(
+                    {
+                        "geom": [],
+                    },
+                    geometry="geom",
+                    crs="EPSG:3857",
+                ),
+                GeoDataFrame(
+                    {
+                        "geometry": [],
+                    },
+                    geometry="geometry",
+                    crs="EPSG:3857",
+                ),
+            ],
+            "Different geometry column names found in GeoDataFrames.",
+        ),
+    ],
+    ids=[
+        "dataframe",
+        "gdf_mixed_with_df",
+        "no_geom_column",
+        "different_crss",
+        "different_geom_col_names",
+    ],
+)
+def test_combine_gdfs_raises(
+    inputs: list[GeoDataFrame],
+    message: str,
+):
+    with pytest.raises(GeoCombineError, match=re.escape(message)):
+        combine_gdfs(inputs)
+
+
+@pytest.mark.parametrize(
+    ("inputs", "kwargs", "output"),
+    [
+        (
+            [
+                GeoDataFrame(geometry=[], crs="EPSG:3857"),
+            ],
+            {},
+            GeoDataFrame(geometry=[], crs="EPSG:3857"),
+        ),
+        (
+            [
+                GeoDataFrame(geometry=[Point(0, 0)], crs="EPSG:3857"),
+            ],
+            {},
+            GeoDataFrame(geometry=[Point(0, 0)], crs="EPSG:3857"),
+        ),
+        (
+            [
+                GeoDataFrame(geometry=[Point(0, 0)], crs="EPSG:3857"),
+                GeoDataFrame(geometry=[Point(1, 0)], crs="EPSG:3857"),
+            ],
+            {},
+            GeoDataFrame(
+                index=[0, 0],
+                geometry=[Point(0, 0), Point(1, 0)],
+                crs="EPSG:3857",
+            ),
+        ),
+        (
+            [
+                GeoDataFrame(
+                    index=[4],
+                    geometry=[Point(0, 0)],
+                    crs="EPSG:3857",
+                ),
+                GeoDataFrame(
+                    index=[7],
+                    geometry=[Point(1, 0)],
+                    crs="EPSG:3857",
+                ),
+            ],
+            {"ignore_index": True},
+            GeoDataFrame(
+                index=[0, 1],
+                geometry=[Point(0, 0), Point(1, 0)],
+                crs="EPSG:3857",
+            ),
+        ),
+    ],
+    ids=[
+        "unchanged_no_features",
+        "unchanged_has_features",
+        "gets_combined",
+        "kwargs",
+    ],
+)
+def test_combine_gdfs_returns(
+    inputs: list[GeoDataFrame],
+    kwargs: ConcatParameters,
+    output: GeoDataFrame,
+):
+    assert_geodataframe_equal(
+        combine_gdfs(inputs, **kwargs),
+        output,
+    )
+
+
+@pytest.mark.parametrize(
+    ("inputs", "message"),
+    [
+        (
+            [Series()],
+            "Non-GeoSeries object found.",
+        ),
+        (
+            [GeoSeries(crs="EPSG:3857"), Series()],
+            "Non-GeoSeries object found.",
+        ),
+        (
+            [
+                GeoSeries(crs="EPSG:3857"),
+                GeoSeries(crs="EPSG:4326"),
+            ],
+            "Different CRSs found in GeoDataFrames.",
+        ),
+        (
+            [
+                GeoSeries(
+                    name="geom",
+                    crs="EPSG:3857",
+                ),
+                GeoSeries(
+                    name="geometry",
+                    crs="EPSG:3857",
+                ),
+            ],
+            "Different geometry column names found in GeoDataFrames.",
+        ),
+    ],
+    ids=[
+        "series",
+        "series_mixed_with_geoseries",
+        "different_crss",
+        "different_geom_col_names",
+    ],
+)
+def test_combine_geoseries_raises(
+    inputs: list[GeoSeries],
+    message: str,
+):
+    with pytest.raises(GeoCombineError, match=re.escape(message)):
+        combine_geoseries(inputs)
+
+
+@pytest.mark.parametrize(
+    ("inputs", "kwargs", "output"),
+    [
+        (
+            [GeoSeries(crs="EPSG:3857")],
+            {},
+            GeoSeries(crs="EPSG:3857"),
+        ),
+        (
+            [
+                GeoSeries([Point(0, 0)], crs="EPSG:3857"),
+            ],
+            {},
+            GeoSeries([Point(0, 0)], crs="EPSG:3857"),
+        ),
+        (
+            [
+                GeoSeries([Point(0, 0)], crs="EPSG:3857"),
+                GeoSeries([Point(1, 0)], crs="EPSG:3857"),
+            ],
+            {},
+            GeoSeries(
+                [Point(0, 0), Point(1, 0)],
+                index=[0, 0],
+                crs="EPSG:3857",
+            ),
+        ),
+        (
+            [
+                GeoSeries(
+                    [Point(0, 0)],
+                    index=[4],
+                    crs="EPSG:3857",
+                ),
+                GeoSeries(
+                    [Point(1, 0)],
+                    index=[7],
+                    crs="EPSG:3857",
+                ),
+            ],
+            {"ignore_index": True},
+            GeoSeries(
+                [Point(0, 0), Point(1, 0)],
+                index=[0, 1],
+                crs="EPSG:3857",
+            ),
+        ),
+    ],
+    ids=[
+        "unchanged_no_features",
+        "unchanged_has_features",
+        "gets_combined",
+        "kwargs",
+    ],
+)
+def test_combine_geries_returns(
+    inputs: list[GeoSeries],
+    kwargs: ConcatParameters,
+    output: GeoSeries,
+):
+    assert_geoseries_equal(
+        combine_geoseries(inputs, **kwargs),
+        output,
+    )
