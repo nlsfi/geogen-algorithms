dc800fe209409da0d9f72a1d73334c20ab89c471
Juho <juho.ervasti@gispo.fi>
2026-03-30 16:01:01 +0300

    refactor(building-areas-parcel): don't save coverage column to output

--- test/application/test_generalize_building_areas_by_parcel.py
+++ test/application/test_generalize_building_areas_by_parcel.py
@@ -27,7 +27,6 @@ def test_generalize_building_areas_by_parcel(testdata_path: Path) -> None:
             buffer_distance=20,
             building_type_column="kohdeluokka",
             building_type_to_select=42211,
-            building_coverage_column="building_coverage",
             reference_key="parcels",
         ),
         unique_id_column=UNIQUE_ID_COLUMN,
